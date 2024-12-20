d1e065d7561cbd5399fa4755bc9ae30b9bd597a5
SidBotz <sidop148@gmail.com>
2024-12-20 21:26:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+API_ID = 5461760
+API_HASH = '396b10bcf5e1ed5fcc71f1603800b7cf'
+BOT_TOKEN = '7277830058:AAGhe3MH-kHxS_M82DZICfupJ7ncuLNgVa0'
+CHANNELS = ["@botxhub", "@sidbotz"]
+XD = 5  # Number of referrals required for Premium
+XDAYS = 7  # Premium duration for referrals (in days)
+API = "1200d6c28a7e63de00fe26ef3d513882c98ff028"
+URL = "linkpays.in"
+
+START_MESSAGE = "Welcome to the Bot Host Service! Join the channels below to proceed."
+AFTER_JOIN_MESSAGE = "Great! Now choose your hosting option below."
